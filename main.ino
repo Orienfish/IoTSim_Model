7b4ec9fc7e81378c20074bb8621d7b75cc7baa8f
Orienfish <x1yu@ucsd.edu>
2019-04-22 16:29:35 -0700

    one file

--- main.ino
+++ main.ino
@@ -6,15 +6,69 @@
  * Author: Orienfish
  * Date: 04/22/2019
  ******************************************************************/
-extern "C" {
-	#include <perf_count.h>
-}
+#define IO_FREQ 16000000
+#define SCALER 1
+#define OVERFLOW 1 << 16
 
 /* Counter frequency */
-extern unsigned long cnt_freq = IO_FREQ / SCALER;
+unsigned long cnt_freq = IO_FREQ / SCALER;
 /* Performance count */
-extern volatile unsigned long count = 0;
+volatile unsigned long count = 0;
 
+/******************************************************************
+ * Set up Timer 1 on Arduino Uno
+ *****************************************************************/
+void Timer1_init(int scaler) {
+  /* Configure the normal mode */
+  TCCR1A = 0;
+  TCCR1B = 0;
+  /* Set frequency scaler */
+  byte scaler_mask = 0;
+  switch(scaler) {
+    case 1: scaler_mask = 0x01; break;
+    case 8: scaler_mask = 0x02; break;
+    case 64: scaler_mask = 0x03; break;
+    case 256: scaler_mask = 0x04; break;
+    case 1024: scaler_mask = 0x05; break;
+    default: break;
+  }
+  TCCR1B |= scaler_mask;
+  Serial.print("Scaler is "); Serial.println(SCALER);
+  Serial.print("Counter frequency is "); Serial.println(cnt_freq);
+  Serial.flush();
+  /* Set counter to 0 */
+  TCNT1 = 0;
+  /* Enable overflow interrupt */
+  TIMSK1 = (1 << TOIE1);
+}
+
+/*****************************************************************
+ * Interrupt service called when timer 1 overflows
+ ****************************************************************/
+ISR(TIMER1_OVF_vect) {
+  count ++;
+}
+
+/*****************************************************************
+ * Read Timer 1 count
+ ****************************************************************/
+unsigned int TIM16_ReadTCNT1(void) {
+  unsigned char sreg;
+  unsigned int i;
+  /* Save global interrupt flag */
+  sreg = SREG;
+  /* Disable interrupts */
+  cli();
+  /* Read TCNT1 */
+  i = TCNT1;
+  /* Restore global interrupt flag */
+  SREG = sreg;
+  return i;
+}
+
+/*****************************************************************
+ * Setup
+ ****************************************************************/
 void setup() {
 	/* start serial at 115200 baud rate */
 	Serial.begin(115200);
@@ -22,6 +76,9 @@ void setup() {
 	Timer1_init(SCALER);
 }
 
+/*****************************************************************
+ * Main loop
+ ****************************************************************/
 void loop() {
   /* Timer 1 starts from here */
   // TCNT1 = 0;
